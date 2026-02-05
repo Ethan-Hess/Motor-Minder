dac868343b577bd84b16a413b936b36d966d8c98
Preston <104221972+plittle1767@users.noreply.github.com>
2026-02-04 23:02:31 -0700

    Add skeleton test files

---
+++ src/tests/test_controller.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+
+class TestController(TestCase):
+    def test_get_service_status(self):
+        self.fail()
+
+    def test_get_vehicles(self):
+        self.fail()
+
+    def test_add_vehicle(self):
+        self.fail()
+
+    def test_edit_vehicle(self):
+        self.fail()
+
+    def test_delete_vehicle(self):
+        self.fail()
+
+    def test_log_service(self):
+        self.fail()
+
+    def test_get_vehicle_services(self):
+        self.fail()
---
+++ src/tests/test_data_handler.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+
+class TestDataHandler(TestCase):
+    def test_load(self):
+        self.fail()
+
+    def test_save(self):
+        self.fail()
+
+    def test_get_vehicles(self):
+        self.fail()
+
+    def test_add_vehicle(self):
+        self.fail()
+
+    def test_update_vehicle(self):
+        self.fail()
+
+    def test_delete_vehicle(self):
+        self.fail()
+
+    def test_log_service(self):
+        self.fail()
