833f1ac110d79a09ff5d0613f4d31c210ea5ed9e
Preston <104221972+plittle1767@users.noreply.github.com>
2026-02-04 22:45:59 -0700

    Add files via upload

---
+++ src/tests/test_cli.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestCLI(TestCase):
+    def test_run(self):
+        self.fail()
+
+    def test_list_vehicles(self):
+        self.fail()
+
+    def test_add_vehicle(self):
+        self.fail()
+
+    def test_edit_vehicle(self):
+        self.fail()
+
+    def test_log_service(self):
+        self.fail()
+
+    def test_maintenance_dashboard(self):
+        self.fail()
---
+++ src/tests/test_models.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+from models import ServiceRecord, Vehicle
+
+oil_change = ServiceRecord(600, "2021-11-11")
+tire_rotation = ServiceRecord(6000, "2025-01-15")
+
+record = ServiceRecord(mileage=60000, date="2022-12-10")
+vehicle = Vehicle("Hyundai", "Elantra", 2023, 62000, {"oil_change": oil_change, "tire_rotation": tire_rotation})
+
+class TestServiceRecord(TestCase):
+    def test_to_dict(self):
+        result = record.to_dict()
+
+        self.assertEqual(result, {"mileage": 60000, "date": "2022-12-10"})
+
+    def test_from_dict(self):
+        self.fail()
+
+
+class TestVehicle(TestCase):
+    def test_to_dict(self):
+        result = vehicle.to_dict()
+
+        expected = {
+            "make": "Hyundai",
+            "model": "Elantra",
+            "year": 2023,
+            "current_mileage": 62000,
+            "last_service": {
+                "oil_change": {"mileage": 600, "date": "2021-11-11"},
+                "tire_rotation": {"mileage": 6000, "date": "2025-01-15"}
+            }
+        }
+
+        assert result == expected
+
+    def test_from_dict(self):
+        self.fail()
