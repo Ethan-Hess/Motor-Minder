ada4991d17027073533553d482b8a4084751873a
Preston <104221972+plittle1767@users.noreply.github.com>
2026-04-04 14:42:02 -0600

    Added updated style sheet and App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,8 +1,13 @@
-import { RouterProvider } from 'react-router-dom';
-import { appRouter } from './app/router.jsx';
+import {RouterProvider} from 'react-router-dom';
+import {appRouter} from './app/router.jsx';
+import {AuthProvider} from './context/AuthContext.jsx';
 
 function App() {
-  return <RouterProvider router={appRouter} />;
+    return (
+        <AuthProvider>
+            <RouterProvider router={appRouter}/>
+        </AuthProvider>
+    );
 }
 
 export default App;
